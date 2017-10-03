6613a0d85b48e5213b62f9a8e5b9ca73a6da739e
Dung Le <dmle@memphis.edu>
2017-10-03 00:31:08 -0500

    add index.php

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Dung Le's CV</title>
+		<link rel = "stylesheet" type = "text/css" href = "pofolio.css">
+		<link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave" rel="stylesheet">
+		
+		<!-- bootstrap link cdn -->
+		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> -->
+		
+	</head>
+	
+	<body>
+	
+		<div id = "top-bar">
+			<div class = "center-body">
+				<img id = "dli-image" src = "image/dli.png">
+				<a class = "table-content left-border" href = "#introduction">Introduction</a>		
+				<a class = "table-content left-border" href = "#projects">Projects</a>		
+				<a class = "table-content left-border" href = "#technology">Technical Skills</a>		
+				<a class = "table-content left-border" href = "#skills">Working Experiences</a>		
+				<a class = "table-content left-border" href = "#education">Education</a>		
+				<a class = "table-content left-border" href = "#honor">Honor &amp; Activities</a>	
+				<a class = "table-content left-border" href = "#contact">Contact</a>		
+
+			</div>
+		</div>
+		<div class = "clear"></div>
+		<div id="left-panel">
+		
+		</div>
+		<!-- <img id = "csimage" src = "image/cs.png" > -->
+		
+		<div id = "intro" class = "center-body">
+			
+			
+			<h3 id = "introduction">Introduction:</h3>
+			<p>
+				<img id = "meandmom-img" src = "image/meandmom.jpg">
+				My name is Dung Manh Le, and I am from Vietnam. I came to the United States to
+					study engineering, and I have honor to receive my first degree, <b>Computer Engineering Technology</b>
+					, at <a href = https://en.wikipedia.org/wiki/Southwest_Tennessee_Community_CollegeSouthwest > 
+					Southwest Tenn Community College </a> in 2016. After my graduation, I decided to pursue
+					higer education at the University of Memphis. I started my first year as an junior in 
+					Engineer Department and studied duel majors, <b>Computer and Electrical Engineering</b>; however
+					after a course of computer engineering field, Introduction to Java Programming, I found 
+					out my passion for programming and software development. For that reason, I switched
+					my major to <b>Computer Science</b> and now concentrating into <b>Cyber Security</b>.
+			</p>
+			
+			<div class = "clear"></div>
+			
+			<hr/>
+			
+			<div id = "education">
+					<h3 >Education:</h3>
+					<ul>
+						<li><a href = "https://en.wikipedia.org/wiki/University_of_Memphis"><b>University of Memphis</b></a>, Memphis, TN – USA. 
+							<span>Expected Graduation <b>May 2019</b></span> <br />
+							Bachelor of Computer and Electrical Engineering<br />
+							<strong>GPA 3.83</strong> 
+						</li>
+
+						<li>
+							Southwest Tennessee Community College, Memphis, TN - USA <br />			            May 2016
+							Associate of Applied Science – Computer Engineering Technology          
+						</li>
+						
+					    <li>High school diploma at <a href = "https://vi.wikipedia.org/wiki/Tr%C6%B0%E1%BB%9Dng_Trung_h%E1%BB%8Dc_ph%E1%BB%95_th%C3%B4ng_chuy%C3%AAn_Tr%E1%BA%A7n_Ph%C3%BA,_H%E1%BA%A3i_Ph%C3%B2ng">Tran Phu Gifted high school</a>, Haiphong city, Vietnam</li>
+
+					</ul>
+								
+				<hr />
+			</div>
+			
+			<div id = "projects">
+				<h3>Projects:</h3>
+					<p>In school, I have worked on multiple class projects applying Java and Python programming 
+					languages. Below are links to my <b>GitHub</b>projects: </p>
+					<ul>
+						<li><p><a href = "https://github.com/DungMLe/Text-based_MineSweeper" target = "_blank"><i>Text-based_MineSweeper</i>
+							<img src = "image/point-finger.jpg">
+						</a></p>
+							<p>Create mine sweeper game using Java object oriented programming. Apply the definition of encapsulation and inheritance of Java programming to create a text based interesting mine sweeper game. </p>
+						</li>
+						
+						<li><a href = "https://github.com/DungMLe/BBCwebpage" target = "_blank"><i>Recreated a part of BBC news/technology webpage</i>
+							<img src = "image/point-finger.jpg">
+						</a>
+							<p>Apply HTML and CSS to create a partial copy of BBC webpage </p>
+						</li>
+						
+						<li><a href = "" target = "_blank">Mapping App</a>
+							<p>Apply Dijkstra’s algorithm, a graph theory, to creat an program that give direction from point A to point B
+							 with the most optimal path </p>
+						</li>
+						
+						<li><a href = "" target = "_blank">Spell Checker</a>
+							<p>Using Binary Search Tree and Trie to construct a spell checker app, which will check the spell of
+							user's input, and if there is a mispell, the app will point out and give some suggestions, which are
+							close to the mispelled words</p>
+						</li>
+					</ul>
+			
+			</div>
+			
+				<hr />	
+				
+				<div class = "clear"></div>
+				
+				<div id = "skills">
+					<h3><li>Technical Skills:</li></h3>
+					<ul>
+						<li><strong>Software:</strong></li>
+						<table>
+							<tr>
+								<td>Java &nbsp</td>
+								<td>Python &nbsp</td>
+								<td>C++ &nbsp</td>
+								<td>HTML5 &nbsp</td>
+								<td>CSS3 &nbsp</td>
+							</tr>
+							<tr>
+								<td>JavaScript &nbsp</td>
+								<td>jQuerry &nbsp</td>
+								<td>Bootstrap4 &nbsp</td>
+								<td>Wordpress &nbsp</td>
+								<td>PHP &nbsp</td>
+							</tr>
+							<tr>
+								<td>MySQL &nbsp</td>
+								<td>MATLAB &nbsp</td>
+								<td>LaTeX &nbsp</td>
+								<td>CMD &nbsp</td>
+								<td>Linux Command Lines &nbsp</td>
+							</tr>
+						</table>
+						<br />
+												
+						<li><strong>Operating System:</strong> <span>Linux, Window, MacOS</span></li>
+						<br />
+						<li><strong>IDEs:</strong><span> &nbsp Eclipse &nbsp Jupyter &nbsp Atom &nbspBlueJ &nbsp
+								CodeBlocks &nbsp</span></li>
+								
+								
+								
+								
+												
+						</table>
+						<br />
+						<li><strong>Design:</strong> <span>AutoCAD, PSpice, Ps &lpar;Adobe Photoshop&rpar;</span></li>
+						<br />
+						<li><strong>Others:</strong> <span>Git, GitHub, Stackoverflow, Google Drive, Dropbox, AppInventor, GameMaker, NXT Minstorm Robot</span></li>
+						
+					
+					</ul>
+					
+				<hr />	
+				</div>
+				
+				<div class = "clear"></div>
+				
+				<div id = "work">
+					<h3 >Working Experience:</h3>
+					<ul>
+						<li>Work as instructor for Bits Bytes & Bots during summer 2017
+							Instruct students to create 2-D games using Game Maker game engine. Instruct students to build Lego Mindstorm Ev3, apply sensors (light sensor and sound sensor), and program the robots to accomplish some tasks and challenge.
+						</li>
+						<br/>
+						<li>Worked as teaching assistance at CodeCrew 
+						Assisted instructor to improve the application using App Inventor to instruct student at Treadwell middle school. Helped and instructed student to finish their projects and applications
+						</li>
+						<br/>
+						<li>Worked in Osaka Japanese Sushi restaurant (since 2015 till present)Responsible for preparing, cooking the ingredients, as well as decorating the plate before delivering to customer. 
+					    Checking inventory and make sure the quality and quantity of ingredients and foods. Instructing and helping the other chefs to prepare the ingredient and making beautiful and delicious sushi.
+						</li>
+					
+					</ul>
+					
+					<hr />	
+				
+				
+				</div>
+				
+				<div id = "honor">
+					<h3 ><li>Honor and Activities: </li></h3>
+					<ul>
+						<li>Google Ignite CS &nbsp
+						<a href = "https://ignitecs.withgoogle.com/" target = "_blank">
+						<img src = "image/google.png" width = "100" title ="click here"></a>
+						&nbsp
+						<a href = "https://www.facebook.com/MemphisComputerScience/posts/597810830420847" target = "_blank">
+						<img src = "image/UoM.png" width = "50" title ="click here"></a>
+						</li>
+						<li>Hack Memphis/Game Jam</li>
+						<li>Member of Tau Beta Pi Engineering Honor Society</li>
+						<li>Member of Association of Computing Machinery (ACM)</li>
+						<li>Summa Cum Laude (Southwest Tennessee Community College)</li>
+						<li>Dean’s List (University of Memphis-2017, Southwest Tennessee Community College-2015,2016)</li>
+						<li>Institute of Electrical and Electronic Engineers 
+						<br />
+							<a href = "https://www.ieee.org/index.html" target = "_blank">
+							<img src = "image/ieee.jpg" width = "50" title ="click here"></a></li>
+
+					</ul>
+				</div>
+				
+				
+
+		</div>
+					
+		
+				<!-- <div id = "contact"> -->
+					<!-- <h3 ><li>Contact Information:</li></h3> -->
+						<!-- <ul> -->
+							<!-- <li><strong>Email:</strong> dmle@memphis.edu</li> -->
+							<!-- <li><strong>Phone:</strong> (510)282-0883</li> -->
+							<!-- <li><strong>LinkeIn:</strong> &nbsp;<a href = "https://www.linkedin.com/in/dung-le-b54752b9/"> -->
+							<!-- <img src = "image/linkeIn.png" width = "25" title = "click here"></a></li> -->
+						<!-- </ul> -->
+				<!-- </div>		 -->
+				<div class = "clear"></div>
+				
+				<div id="contact">
+					<span>Contact Information:</span>
+					
+					  <div class="row">
+						<div class="col">
+							<li><strong>Email:</strong> dmle@memphis.edu</li>
+						</div>
+						
+						<div class="col">
+							<li><strong>Phone:</strong> (510)282-0883</li>
+						</div>
+						
+						<div class="col">
+							<li><strong>LinkedIn:</strong> &nbsp;<a href = "https://www.linkedin.com/in/dung-le-b54752b9/">
+							<img src = "image/linkeIn.png" width = "25" title = "click here"></a></li>
+						</div>
+						<div class="col">
+							
+						</div>
+					  </div>
+				</div>
+				
+			
+	</body>
+</html>
